2f310c4f3415eb5d89e54206f5a2d6a824043470
Eliza <elizapetrycka@gmail.com>
2025-01-05 21:26:41 +0100

    update

--- UserService/app/services/auth.py
+++ UserService/app/services/auth.py
@@ -13,7 +13,7 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 
 def create_refresh_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(days=settings.REFRESH_TOKEN_EXPIRE_DAYS)
+    expire = datetime.utcnow() + timedelta(days=int(settings.REFRESH_TOKEN_EXPIRE_DAYS))
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
